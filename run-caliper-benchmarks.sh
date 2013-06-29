bb2ceb3728d832e9fe35952bbbcec1fc3ad8f82b
Mike Sokolov <mikhail.g.sokolov@gmail.com>
2013-06-29 19:53:21 +0700

    Modify run-caliper-benchmarks script

--- run-caliper-benchmarks.sh
+++ run-caliper-benchmarks.sh
@@ -1,4 +1,5 @@
-git pull
+git fetch origin
+git checkout origin/new-hashset-impl
 ant
 mvn install:install-file -Dfile=./build/pack/lib/scala-library.jar -DgroupId=com.softage -DartifactId=scala-library -Dversion=2.11.SNAPSHOT -Dpackaging=jar
 cd ./caliper_benchmarks
