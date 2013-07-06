a72cba07b7d1a7c42fb7b070cf801603812fb914
Mike Sokolov <mikhail.g.sokolov@gmail.com>
2013-07-06 13:22:25 +0700

    Remove from the caliper benchmark script all the Git manipulations

--- run-caliper-benchmarks.sh
+++ run-caliper-benchmarks.sh
@@ -1,5 +1,3 @@
-git fetch origin
-git checkout origin/new-hashset-impl
 ant
 mvn install:install-file -Dfile=./build/pack/lib/scala-library.jar -DgroupId=com.softage -DartifactId=scala-library -Dversion=2.11.SNAPSHOT -Dpackaging=jar
 cd ./caliper_benchmarks
