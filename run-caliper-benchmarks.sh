768adb57dfa91e93c0ffb67888011de980bc9c1f
Mike Sokolov <mikhail.g.sokolov@gmail.com>
2013-07-06 16:18:56 +0700

    Add actual caliper benchmarks run command to the script

--- run-caliper-benchmarks.sh
+++ run-caliper-benchmarks.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
 ant
 mvn install:install-file -Dfile=./build/pack/lib/scala-library.jar -DgroupId=com.softage -DartifactId=scala-library -Dversion=2.11.SNAPSHOT -Dpackaging=jar
 cd ./caliper_benchmarks
 mvn -U clean install
+java -jar target/benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar -l 0
