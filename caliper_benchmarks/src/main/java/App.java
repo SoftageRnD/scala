3377df5a8153dfddc98542821239abaa8c1ccc08
Mike Sokolov <mikhail.g.sokolov@gmail.com>
2013-07-20 16:37:43 +0700

    Modify the caliper benchmark runner to run multiple benchmarks

--- caliper_benchmarks/src/main/java/App.java
+++ caliper_benchmarks/src/main/java/App.java
@@ -1,14 +1,24 @@
+import com.google.caliper.Benchmark;
 import com.google.caliper.runner.CaliperMain;
 
+import java.util.Arrays;
 
-/**
- * Hello world!
- * 
- */
 public class App {
-	public static void main(String[] args) {
-    //    CaliperMain.main(RemoveHashSetBenchmark.class, args);
-        CaliperMain.main(AddHashSetBenchmark.class, args);
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        if (args == null || args.length < 1) {
+            System.out.println("you must supply at least one argument - benchmark class full qualified name");
+            System.exit(-1);
+        }
+        String benchmarkClassName = args[0];
+        String[] caliperArgs = Arrays.copyOfRange(args, 1, args.length);
+        try {
+            Class<? extends Benchmark> benchmarkClass = Class.forName(benchmarkClassName).asSubclass(Benchmark.class);
+            System.out.println("Benchmark starting: " + benchmarkClassName);
+            CaliperMain.main(benchmarkClass, caliperArgs);
+        } catch (ClassNotFoundException e) {
+            System.out.println("unknown benchmark class: " + benchmarkClassName);
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
--- run-caliper-benchmarks.sh
+++ run-caliper-benchmarks.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+BENCHMARK_LIST=('AddHashSetBenchmark' 'ContainsHashSetBenchmark' 'RemoveHashSetBenchmark')
 
 set -e
 ant
 mvn install:install-file -Dfile=./build/pack/lib/scala-library.jar -DgroupId=com.softage -DartifactId=scala-library -Dversion=2.11.SNAPSHOT -Dpackaging=jar
 cd ./caliper_benchmarks
 mvn -U clean install
-java -jar target/benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar -l 0
+
+for benchmark in "${BENCHMARK_LIST[@]}"
+do
+	java -jar target/benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar $benchmark -l 0
+done
