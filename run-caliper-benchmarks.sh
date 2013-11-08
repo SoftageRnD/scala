187524cea0d353afc68477b030e3f2bc6d97e720
unknown <j.kostyukova@BETA.SOFTAGE>
2013-11-08 18:34:52 +0700

    Added caliper instrument option to collect only runtime results from benchmarks

--- run-caliper-benchmarks.sh
+++ run-caliper-benchmarks.sh
@@ -5,4 +5,4 @@ ant
 mvn install:install-file -Dfile=./build/pack/lib/scala-library.jar -DgroupId=com.softage -DartifactId=scala-library -Dversion=2.11.SNAPSHOT -Dpackaging=jar
 cd ./caliper_benchmarks
 mvn -U clean install
-java -jar target/benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar -l 0
+java -jar target/benchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar -l 0 -i micro
